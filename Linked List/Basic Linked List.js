daefc9780bf0e60954f48b922100875116cc0d7c
Yuviii <Yuvrajmule111@gmail.com>
2022-11-23 18:33:14 +0530

    Todo App Proccesing

---
+++ Linked List/Basic Linked List.js
@@ -0,0 +1,40 @@
+// st Linked list here
+
+function Node(number) {
+    this.number = number;
+    this.next = null;
+}
+
+class LinkedList {
+    head;
+
+    addNodeAtLast(number) {
+        let node = new Node(number);
+        if(!this.head) {
+            this.head = node;
+        } else {
+            let current = this.head;
+            while(current.next !== null) {
+                current = current.next;
+            }
+            current.next = node;
+        }
+    }
+
+    printList() {
+        let current = this.head;
+        let str = '';
+        while(current !== null) {
+            str+=`&{current.number}-->`;
+            current = current.next;
+        }
+        console.log(str);
+    }
+}
+
+const list = new LinkedList();
+list.addNodeAtLast(4);
+list.addNodeAtLast(6);
+list.addNodeAtLast(1);
+list.addNodeAtLast(10);
+list.printList();
