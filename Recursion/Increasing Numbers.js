e0dd44ab3696c10d34d3e0ad269364499f60ea5b
Yuviii <Yuvrajmule111@gmail.com>
2022-12-01 19:54:27 +0530

    updated numbers

---
+++ Recursion/Increasing Numbers.js
@@ -0,0 +1,9 @@
+function f(n) {
+    if(n == 1) {
+        console.log(1);
+        return ;
+    }
+    if(n - 1); // print the first elelment 
+    let  n = 4;
+    console.log(n); // printing myself
+}
